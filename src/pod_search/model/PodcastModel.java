c5f9f20253421349fee749cad2346f35b6d6a5eb
Patrick Chen <patrickc3832@gmail.com>
2012-05-04 14:50:37 +0200

    remove project files

--- src/pod_search/model/PodcastModel.java
+++ src/pod_search/model/PodcastModel.java
@@ -4,9 +4,25 @@ package pod_search.model;
 
 public class PodcastModel {
 	
-	private String name;
-	private String link;
-	private int id;
+	private String _name;
+	private String _link;
+	private int _id;
 
-	
+	public PodcastModel(String name, String link, int id = NULL) {
+		setName(name);
+		setLink(link);
+		setID(id);
+	}
+
+	public void setName(String name) {
+		_name = name;
+	}
+
+	public void setLink(String link) {
+		_link = link;
+	}
+
+	public void setID(int id) {
+		_id = id;
+	}
 }
